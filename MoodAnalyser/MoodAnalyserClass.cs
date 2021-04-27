7f638cdb741de79d6121c07c8a6fd5495add3d2c
santanu619 <mohapatra.santanu123@gmail.com>
2021-04-27 13:03:01 +0530

    Added: Code for MoodAnalyser

---
+++ MoodAnalyser/MoodAnalyserClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyserClass
+    {
+        private string message;
+        public MoodAnalyserClass(string message)
+        {
+            this.message = message;
+        }
+        public MoodAnalyserClass()
+        {
+        }
+        public string analyseMoodLive1()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+        }
+    }
+
+}
